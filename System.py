537196768e72ca451998bfab0a71b6316857e065
genius <ewoudketele@protonmail.com>
2020-02-25 16:10:21 +0100

    Commit

---
+++ System.py
@@ -0,0 +1,95 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('fivethirtyeight')
+kleur = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+Propensities = np.array([[0, -1, -2], [-1, 0, -3], [-2, -3, 0]])
+
+
+class System:
+
+    def __init__(self, config):
+
+        self.config = np.array(config)
+        self.Dim = self.config.size
+
+        self.parent = None
+        self.children = None
+
+        self.gain = self.hamiltonian(propensities=Propensities)
+
+    def __repr__(self):
+
+        temp = str(self.config)
+        return temp
+
+    def __getitem__(self, key):
+
+        return self.config[key]
+
+    def __setitem__(self, key, value):
+
+        self.config[key] = value
+
+    def __mul__(self, other):
+
+        temp = np.copy(self.config)
+        temp *= other
+        return System(temp)
+
+    def __rmul__(self, other):
+
+        temp = np.copy(self.config)
+        temp *= other
+        return System(temp)
+
+    def __eq__(self, other):
+
+        return np.array_equal(self.config, other.config)
+
+    def __hash__(self):
+        return hash(self.config.tostring())
+
+    def invert(self):
+
+        temp = np.copy(self.config)
+        temp *= -1
+
+        new_config = System(temp)
+
+        return new_config
+
+    def flip(self, actor):
+
+        temp = np.copy(self.config)
+        temp[actor.index] *= -1
+
+        new_config = System(temp)
+
+        return new_config
+
+    def hamiltonian(self, propensities):
+
+        h = np.zeros(self.Dim)
+
+        transpose = self.config.reshape((-1, 1))
+        for i in range(self.Dim):
+            temp = np.zeros(self.Dim)
+            temp[i] = self[i]
+            h[i] = temp.dot(propensities).dot(transpose)[0]
+
+        return h
+
+    def toJson(self):
+        if self.children is None:
+            return {
+                'config': self.config.tolist(),
+                'gain': self.gain.tolist()
+                    }
+        else:
+            return {
+                'children': [child.toJson() for child in self.children],
+                'config': self.config.tolist(),
+                'gain': self.gain.tolist()
+            }
