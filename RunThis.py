849828d2c07d1ee0de328f69863151bbd3912d1e
genius <ewoudketele@protonmail.com>
2020-02-25 18:03:48 +0100

    Changes

--- RunThis.py
+++ RunThis.py
@@ -6,8 +6,10 @@ Spain = Actor.Actor('Spain', 1, -1)
 France = Actor.Actor('France', 2, -1)
 
 Start = System.System([1, 1, 1])
-France.make_tree(Start)
+
 England.make_tree(Start)
+Spain.make_tree(Start)
+France.make_tree(Start)
 
 print(England.tree)
 
