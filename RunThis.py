63bb05a80bb45eec1eb3f7aabfc406adfaa149db
genius <ewoudketele@protonmail.com>
2020-02-26 15:31:17 +0100

    sure

--- RunThis.py
+++ RunThis.py
@@ -1,3 +1,4 @@
+import numpy as np
 import Tree
 import Actor as Actor
 import System as System
@@ -5,12 +6,12 @@ import System as System
 England = Actor.Actor('England', 0, -1)
 Spain = Actor.Actor('Spain', 1, -1)
 France = Actor.Actor('France', 2, -1)
+Prussia = Actor.Actor('Prussia', 3, -1)
 
-Start = System.System([1, 1, 1])
+Start = System.System(np.zeros(4).tolist())
 
 England.make_tree(Start)
-Spain.make_tree(Start)
-France.make_tree(Start)
+
 print(England.tree)
 
 print('done')
