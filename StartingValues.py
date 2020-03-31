579e3dbc93194c61ba93a1a3bb1b6958a280129d
genius <ewoudketele@protonmail.com>
2020-03-31 23:32:16 +0200

    Introduce new actors

--- StartingValues.py
+++ StartingValues.py
@@ -2,13 +2,12 @@ import numpy as np
 from System import System
 from Actor import Actor
 
-One = Actor('USA', -1, [0, 1, 1, 1, 1, -1], belonging=-1)
-Two = Actor('UK', -1, [1, 0, -1, 1, 1, -1], belonging=-1)
-Three = Actor('France', -1, [1, -1, 0, 1, -1, -1], belonging=-1)
-Four = Actor('USSR', -1, [1, 1, 1, 0, -1, -1], belonging=-1)
-Five = Actor('DDR', -1, [1, 1, -1, -1, 0, -1], belonging=1)
-Six = Actor('USSR', -1, [-1, -1, -1, -1, -1, 0], belonging=1)
+One = Actor('USA', -1,     [0, 1, 1, 1, -1], belonging=-1)
+Two = Actor('UK', -1,      [1, 0, 1, 1, -1], belonging=-1)
+Three = Actor('France', -1, [1, 1, 0, -1, -1], belonging=-1)
+Four = Actor('DDR', -1,    [1, 1, -1, 0, -1], belonging=1)
+Five = Actor('USSR', -1,    [-1, -1, -1, -1, 0], belonging=1)
 
-Actors = [One, Two, Six]
+Actors = [One, Two, Three, Four, Five]
 
 Start = System(np.ones(len(Actors)), Actors)
