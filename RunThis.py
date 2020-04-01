efdce43db25f4c0313846b886a9b893599f464f6
genius <ewoudketele@protonmail.com>
2020-04-01 13:26:47 +0200

    minor changes

--- RunThis.py
+++ RunThis.py
@@ -6,6 +6,7 @@ from Loose import write_evolution
 def Simulation(start):
     local_optimum_counter = 0
     counter = 0
+    unstable = False
 
     while local_optimum_counter < 10:
 
@@ -28,11 +29,13 @@ def Simulation(start):
 
         counter += 1
 
-        if counter == 20:
+        if counter == 50:
             local_optimum_counter = 11
             print('unstable system')
+            unstable = True
 
-    print('stable configuration found')
+    if not unstable:
+        print('stable configuration found')
     write_evolution(start)
     return start
 
