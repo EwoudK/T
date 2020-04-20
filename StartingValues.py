27f974e48cfe4ef12be62e0d921f47f3d1d6cae0
genius <ewoudketele@protonmail.com>
2020-04-20 13:53:56 +0200

    minor changes

--- StartingValues.py
+++ StartingValues.py
@@ -2,11 +2,9 @@ import numpy as np
 from System import System
 from Actor import Actor
 
-One = Actor('USA', -1,     [0, 1, 1, 1, -1], belonging=-1)
-Two = Actor('UK', -1,      [1, 0, 1, 1, -1], belonging=-1)
-Three = Actor('France', -1, [1, 1, 0, -1, -1], belonging=-1)
-Four = Actor('DDR', -1,    [1, 1, -1, 0, -1], belonging=1)
-Five = Actor('USSR', -1,    [-1, -1, -1, -1, 0], belonging=1)
+One = Actor('USA', 0,     [0, 1, 1], belonging=1)
+Two = Actor('UK', 0,      [1, 0, 1], belonging=-1)
+Three = Actor('France', 0, [1, 1, 0], belonging=-1)
 
 Actors = [One, Two, Three]
 
