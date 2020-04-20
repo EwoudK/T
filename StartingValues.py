615c90a585a08f4adea75a9c7e8209c6c24bdc06
genius <ewoudketele@protonmail.com>
2020-04-20 09:59:11 +0200

    minor changes

--- StartingValues.py
+++ StartingValues.py
@@ -8,6 +8,6 @@ Three = Actor('France', -1, [1, 1, 0, -1, -1], belonging=-1)
 Four = Actor('DDR', -1,    [1, 1, -1, 0, -1], belonging=1)
 Five = Actor('USSR', -1,    [-1, -1, -1, -1, 0], belonging=1)
 
-Actors = [One, Two, Three, Four, Five]
+Actors = [One, Two, Three]
 
 Start = System(np.ones(len(Actors)), Actors)
