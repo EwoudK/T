69db10ed58423eaf383c0e02b009803a06a7af62
genius <ewoudketele@protonmail.com>
2020-05-07 11:23:39 +0200

    import prefactor function

--- RUNFILE.py
+++ RUNFILE.py
@@ -1,11 +1,12 @@
 from System import System
 from StartingValues import Actors, SpinValues
 from Loose import config_to_Json, print_energy_degeneracy, Simulation
+from numpy import heaviside
 
-Test = System(Actors, SpinValues, Hamiltonian_to_use='B')
+Test = System(Actors, SpinValues, Hamiltonian_to_use='V')
 
 print_energy_degeneracy(Test)
 
-Simulation(Test, update='chronological')
+Simulation(Test, update='chronological', prefactor_function=heaviside)
 
 config_to_Json(Test)
