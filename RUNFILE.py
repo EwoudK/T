cd16668923e76bdf0042d5f43b1f943e01ccdd89
genius <ewoudketele@protonmail.com>
2020-05-20 13:51:07 +0200

    small changes

--- RUNFILE.py
+++ RUNFILE.py
@@ -3,10 +3,10 @@ from StartingValues import Actors, SpinValues
 from Loose import config_to_Json, print_energy_degeneracy, Simulation
 from numpy import heaviside
 
-Test = System(Actors, SpinValues, Hamiltonian_to_use='V')
+Test = System(Actors, SpinValues, Hamiltonian_to_use='B')
 
 print_energy_degeneracy(Test)
 
-Simulation(Test, update='chronological', prefactor_function=heaviside)
+Simulation(Test, update='chronological', prefactor_function=None)
 
 config_to_Json(Test)
